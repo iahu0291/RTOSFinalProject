5e9fb4f8901acbfca756c8fd85842cb0e2bdbcf0
iahu0291 <iahu0291@colorado.edu>
2021-11-05 18:37:00 -0600

    Moved thrust structures and prototypes to thrust.h, where they belong

--- FinalProject/src/Header_Files/thrust.h
+++ FinalProject/src/Header_Files/thrust.h
@@ -8,6 +8,19 @@
 #ifndef SRC_HEADER_FILES_THRUST_H_
 #define SRC_HEADER_FILES_THRUST_H_
 
+enum thrust_possibilities{
+  thrust_none = 0,
+  thrust_min = 1,
+  thrust_max = 2
+};
 
+struct craft_thrust_struct {
+  int current_thrust;
+  int current_fuel;
+};
+
+// Function Prototypes
+
+void update_thrust_data(struct craft_thrust_struct* craft_thrust_data, int new_thrust);
 
 #endif /* SRC_HEADER_FILES_THRUST_H_ */
--- FinalProject/src/Source_Files/thrust.c
+++ FinalProject/src/Source_Files/thrust.c
@@ -5,14 +5,5 @@
  *      Author: Lena
  */
 
-enum thrust_possibilities{
-  thrust_none = 0,
-  thrust_min = 1,
-  thrust_max = 2
-};
 
-struct craft_thrust_struct {
-  int current_thrust;
-  int current_fuel;
-};
 
