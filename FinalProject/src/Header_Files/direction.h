38f56e89bd622742da4c7b5b293bf65a2e6ba926
iahu0291 <iahu0291@colorado.edu>
2021-11-05 18:37:32 -0600

    Moved direction structs to header file where they belong

--- FinalProject/src/Header_Files/direction.h
+++ FinalProject/src/Header_Files/direction.h
@@ -8,6 +8,18 @@
 #ifndef SRC_HEADER_FILES_DIRECTION_H_
 #define SRC_HEADER_FILES_DIRECTION_H_
 
+enum rotational_thrust_options{
+  hard_ccw = -2,
+  soft_ccw = -1,
+  no_rot = 0,
+  soft_cw = 1,
+  hard_cw = 2
+};
 
+struct craft_direction_struct{
+  int currentDirection; // Current angle in degrees
+  int currentAngMomentum; //How fast the ship is currently spinning, positive is CW and negative is CCW
+  int rotational_thrust; //Which rotational direction and how hard are we adjusting spin
+};
 
 #endif /* SRC_HEADER_FILES_DIRECTION_H_ */
--- FinalProject/src/Source_Files/direction.c
+++ FinalProject/src/Source_Files/direction.c
@@ -4,16 +4,4 @@
  *  Created on: Nov 5, 2021
  *      Author: Lena
  */
-enum rotational_thrust_options{
-  hard_ccw = -2,
-  soft_ccw = -1,
-  no_rot = 0,
-  soft_cw = 1,
-  hard_cw = 2
-};
 
-struct craft_direction_struct{
-  int currentDirection; // Current angle in degrees
-  int currentAngMomentum; //How fast the ship is currently spinning, positive is CW and negative is CCW
-  int rotational_thrust; //Which rotational direction and how hard are we adjusting spin
-};
