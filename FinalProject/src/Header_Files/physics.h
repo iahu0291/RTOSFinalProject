cb0d0e53ecedd24f47b3a3d74eeff88fb53bba40
iahu0291 <iahu0291@colorado.edu>
2021-11-12 20:07:26 -0700

    Added physics prototypes

--- FinalProject/src/Header_Files/physics.h
+++ FinalProject/src/Header_Files/physics.h
@@ -10,5 +10,11 @@
 
 #include "structs.h"
 
+// Function Prototypes
+void tick_update_position(struct craft_thrust_struct *thrust_data, struct craft_direction_struct *direction_data, struct craft_position_struct *position_data, struct game_settings_struct *settings);
+
+void tick_burn_fuel(struct craft_thrust_struct *thrust_data, struct craft_direction_struct *direction_data, struct game_settings_struct *settings);
+
+int check_landing(struct craft_position_struct *position_data, struct game_settings_struct *settings);
 
 #endif /* SRC_HEADER_FILES_PHYSICS_H_ */
