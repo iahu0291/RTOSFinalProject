d31ac0be77ec3c2357d28a7088bead26c8ca1f81
iahu0291 <iahu0291@colorado.edu>
2021-11-19 14:08:58 -0700

    Update ledfunc.c
    Created function to update LED control struct

--- FinalProject/src/Source_Files/ledfunc.c
+++ FinalProject/src/Source_Files/ledfunc.c
@@ -5,4 +5,9 @@
  *      Author: Lena
  */
 
+//dutyCycle scales from 0-64, with 64 being a 100% duty cycle and 0 being 0%
+void update_led_control_struct(struct led_control_struct *led_ctrl, int period, int dutyCycle){
+  led_ctrl->restartPeriod = period;
 
+  led_ctrl->timeFromOnToOff = ((period * dutyCycle) >> 6);
+}
