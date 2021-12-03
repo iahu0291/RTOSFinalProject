ecebcdefc2f8fdc8c0ac0a3ef44662c8588da571
iahu0291 <iahu0291@colorado.edu>
2021-12-03 11:30:31 -0700

    Update physics.c
    Added blackout

--- FinalProject/src/Source_Files/physics.c
+++ FinalProject/src/Source_Files/physics.c
@@ -77,43 +77,50 @@ static const int yMultVals[] = {200,
 void tick_update_position(struct craft_thrust_struct *thrust_data, struct craft_direction_struct *direction_data, struct craft_position_struct *position_data, struct game_settings_struct *settings){
 
   // Calculate new rotation rate from angular thrust
-  switch(direction_data->rotational_thrust){
-    case hard_ccw:
-      direction_data->current_rotation_rate -= settings->rotationSpeedQuantaMax;
-      break;
-    case soft_ccw:
-      direction_data->current_rotation_rate -= settings->rotationSpeedQuantaMin;
-      break;
-    case soft_cw:
-      direction_data->current_rotation_rate += settings->rotationSpeedQuantaMin;
-      break;
-    case hard_cw:
-      direction_data->current_rotation_rate += settings->rotationSpeedQuantaMax;
-      break;
-  }
-
-
-  // Calculate new rotation angle from angular rotation rate
-  int nextDirection = direction_data->current_direction + direction_data->current_rotation_rate;
-  if(nextDirection >= 256){
-    nextDirection -= 256;
-  } // nextDirection will range from 0 - 255. 0 will represent pointing right
-
-//  direction_data->current_direction = nextDirection;
-  update_direction_data(direction_data, nextDirection);
-
-
-  // Calculate new velocity from thrust, direction, & gravity
-  int thrustOutput = (thrust_data->current_thrust * settings->maxThrust) >> 1;
   int xAccel = 0;
   int yAccel = 0;
-  int totalMass = settings->vehicleMass + thrust_data->current_fuel;
-  // TODO: Make a final choice: Divide by totalMass b4 function, reducing accuracy,
-  // Or divide by totalMass after function twice, reducing speed.
-  divide_thrust(thrustOutput / totalMass, direction_data->current_direction, &xAccel, &yAccel);
+  if(!thrust_data->blacked_out){
+    switch(direction_data->rotational_thrust){
+      case hard_ccw:
+        direction_data->current_rotation_rate -= settings->rotationSpeedQuantaMax;
+        break;
+      case soft_ccw:
+        direction_data->current_rotation_rate -= settings->rotationSpeedQuantaMin;
+        break;
+      case soft_cw:
+        direction_data->current_rotation_rate += settings->rotationSpeedQuantaMin;
+        break;
+      case hard_cw:
+        direction_data->current_rotation_rate += settings->rotationSpeedQuantaMax;
+        break;
+    }
+
+
+    // Calculate new rotation angle from angular rotation rate
+    int nextDirection = direction_data->current_direction + direction_data->current_rotation_rate;
+    if(nextDirection >= 256){
+      nextDirection -= 256;
+    } // nextDirection will range from 0 - 255. 0 will represent pointing right
+
+  //  direction_data->current_direction = nextDirection;
+    update_direction_data(direction_data, nextDirection);
+
+
+    // Calculate new velocity from thrust, direction, & gravity
+    int thrustOutput = (thrust_data->current_thrust * settings->maxThrust) >> 1;
+    int totalMass = settings->vehicleMass + thrust_data->current_fuel;
+    // TODO: Make a final choice: Divide by totalMass b4 function, reducing accuracy,
+    // Or divide by totalMass after function twice, reducing speed.
+    divide_thrust(thrustOutput / totalMass, direction_data->current_direction, &xAccel, &yAccel);
+  }
   position_data->current_x_vel += xAccel;
   position_data->current_y_vel += yAccel - settings->gravity;
 
+  if((xAccel^2)+(yAccel^2) >= settings->blackoutAccel){
+      // Start Blackout Timer
+
+      thrust_data->blacked_out = 1;
+  }
 
   // Calculate new position from velocity
   position_data->current_x_position += position_data->current_x_vel;
@@ -162,7 +169,7 @@ unsigned int get_led_ctrls(struct craft_thrust_struct *thrust_data, struct game_
   }
   returnVal += led1_period << LED1_PERIOD_SHIFT;
   returnVal += led1_dutyCycle << LED1_DUTY_CYCLE_SHIFT;
-  returnVal += led0_period << 6;
+  returnVal += led0_period << LED0_PERIOD_SHIFT;
   returnVal += led0_dutyCycle;
 
   return returnVal;
